62ddd4995aed8f454e15289b1c3987964c719985
TheCyberSavior01 <Limon.hossain26@yahoo.com>
2023-07-05 17:11:38 +0600

    finalized with dynamic data

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const posts = [
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: 21
+        likes: "21,235"
     },
     {
         name: "Gustave Courbet",
@@ -15,7 +15,7 @@ const posts = [
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        likes: "4,095"
     },
         {
         name: "Joseph Ducreux",
@@ -24,7 +24,38 @@ const posts = [
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        likes: "1,52,023"
     }
 ]
 
+const mainBodyEl = document.getElementById("main-body")
+
+for (let i = 0; i < posts.length; i++) {
+    let content =  `
+        <section>
+            <div class="user-info">
+                <img src=${posts[i].avatar} alt="avatar of vangogh" class="user-avatar" >
+                <div class="user-details">
+                    <h4>${posts[i].name}</h4>
+                    <p>${posts[i].location}</p>
+                </div>
+            </div>
+        <div class="container">
+            <img src=${posts[i].post} alt="post of vangogh" class="post-img">
+        </div>
+            <div class="body">
+                <div class="icons">
+                    <img src="images/icon-heart.png" class="icon">
+                    <img src="images/icon-comment.png" class="icon">
+                    <img src="images/icon-dm.png" class="icon">
+                </div>
+                <p class="like-count">${posts[i].likes} likes</p>
+                <p class="comment"><span class="commenter">${posts[i].username}</span> ${posts[i].comment}</p>
+            </div>
+        <div class="blank-space"></div>
+        </section>
+    `
+
+    mainBodyEl.innerHTML += content
+    
+}
